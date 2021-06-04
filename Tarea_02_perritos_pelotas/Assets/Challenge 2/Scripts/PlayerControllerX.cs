c5ca44eec681024dfd6bcb952ea6adfa55024231
Luis ángel renes diez <46091077+lardas73@users.noreply.github.com>
2021-06-04 18:08:22 +0200

    Tarea 02

---
+++ Tarea_02_perritos_pelotas/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+ 
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float ratio = 1f;
+    private float ultimoRatio = 0;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        ultimoRatio += Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && ultimoRatio > ratio)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            ultimoRatio = 0;
+        }
+    }
+}
